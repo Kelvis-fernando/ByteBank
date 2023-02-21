27e426cf5daa45900731c6c681d4970a4b5f5d52
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-02-21 15:04:37 -0300

    Created a method to change values inside class

--- POO Alura/ContaCorrente.cs
+++ POO Alura/ContaCorrente.cs
@@ -1,7 +1,22 @@
-﻿public class ContaCorrente
+﻿using System;
+
+public class ContaCorrente
 {
     public string titular;
     public int numero;
     public int agencia;
-    public double saldo;
+    public double saldo = 100.00;
+
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
 }
--- POO Alura/Program.cs
+++ POO Alura/Program.cs
@@ -12,14 +12,25 @@ namespace POO_Alura
         {
             ContaCorrente contaKelvis = new ContaCorrente();
             contaKelvis.titular = "Kelvis";
-            contaKelvis.saldo = 1234.5;
             contaKelvis.numero = 08754;
             contaKelvis.agencia = 865;
+            contaKelvis.saldo = 1000.50;
 
             Console.WriteLine(contaKelvis.titular);
             Console.WriteLine(contaKelvis.numero);
             Console.WriteLine(contaKelvis.agencia);
             Console.WriteLine(contaKelvis.saldo);
+
+            bool resultadoDoSaque = contaKelvis.Sacar(100);
+
+            if (resultadoDoSaque)
+            {
+                Console.WriteLine("O seu saldo atual e: $" + contaKelvis.saldo);
+            }
+            else
+            {
+                Console.WriteLine("O seu saldo e insuficiente, o valor atual e de: $" + contaKelvis.saldo);
+            }
 
             Console.ReadLine();
         }
